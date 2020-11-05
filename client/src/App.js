b1b2a12cbed5077bf7e4456950f20e647b1a4de8
Maxx Sanner <maxxsanner105@gmail.com>
2020-11-04 19:30:29 -0800

    added all files

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './pages/Home';
+import Item from './pages/Item';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import UserProfile from './pages/UserProfile';
+import About from './pages/About';
+import NoMatch from './pages/NoMatch';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
+    <Router>
     <div>
-      <h1>TEST</h1>
+      <Navbar/>
+      <Switch>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/item' component={Item}></Route>
+        <Route exact path='/login' component={Login}></Route>
+        <Route exact path='/signup' component={Signup}></Route>
+        <Route exact path='/about' component={About}></Route>
+        <Route exact path='/userProfile' component={UserProfile}></Route>
+        <Route path='*' component={NoMatch}></Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Signup() {
+    return (
+        <div>
+            <h1>SIGNUP</h1>
+        </div>
+    );
+}
+
+export default Signup;
