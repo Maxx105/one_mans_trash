47f40c21e73bd80abe83732beadbee1d58e776c9
Fernando Angulo Donoso <fangulo82@gmail.com>
2020-11-21 22:23:51 -0800

    Added homePage filter functionality

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -117,188 +117,24 @@ function Home() {
     function handleLocationInputChange(e) {
         setLocation(e.target.value)
     }
-    // const zipcodeData = [
-    //         {
-    //             zip_code: "92659",
-    //             distance: 8.77,
-    //             city: "Newport Beach",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92663",
-    //             distance: 8.145,
-    //             city: "Newport Beach",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92660",
-    //             distance: 9.842,
-    //             city: "Newport Beach",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92628",
-    //             distance: 8.071,
-    //             city: "Costa Mesa",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92627",
-    //             distance: 7.297,
-    //             city: "Costa Mesa",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92615",
-    //             distance: 5.782,
-    //             city: "Huntington Beach",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92646",
-    //             distance: 4.812,
-    //             city: "Huntington Beach",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92648",
-    //             distance: 3.07,
-    //             city: "Huntington Beach",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92626",
-    //             distance: 6.503,
-    //             city: "Costa Mesa",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92707",
-    //             distance: 7.989,
-    //             city: "Santa Ana",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92605",
-    //             distance: 1.039,
-    //             city: "Huntington Beach",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92728",
-    //             distance: 5.185,
-    //             city: "Fountain Valley",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92799",
-    //             distance: 6.323,
-    //             city: "Santa Ana",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92735",
-    //             distance: 9.167,
-    //             city: "Santa Ana",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92708",
-    //             distance: 3.406,
-    //             city: "Fountain Valley",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "90742",
-    //             distance: 3.945,
-    //             city: "Sunset Beach",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92704",
-    //             distance: 5.961,
-    //             city: "Santa Ana",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92647",
-    //             distance: 0,
-    //             city: "Huntington Beach",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92649",
-    //             distance: 2.474,
-    //             city: "Huntington Beach",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "90743",
-    //             distance: 4.681,
-    //             city: "Surfside",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92702",
-    //             distance: 7.982,
-    //             city: "Santa Ana",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92655",
-    //             distance: 2.052,
-    //             city: "Midway City",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92703",
-    //             distance: 6.058,
-    //             city: "Santa Ana",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92701",
-    //             distance: 8.797,
-    //             city: "Santa Ana",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "90853",
-    //             distance: 7.221,
-    //             city: "Long Beach",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92685",
-    //             distance: 2.148,
-    //             city: "Westminster",
-    //             state: "CA"
-    //         },
-    //         {
-    //             zip_code: "92712",
-    //             distance: 8.179,
-    //             city: "Santa Ana",
-    //             state: "CA"
-    //         }
-    //  ]
+
     function handleDistanceInputChange(e) {
         let distanceArray = [];
-        ZipAPI.getZipCodesByRadius(location, e.target.value).then(data=>{
-            data.data.zip_codes.forEach(zip_code => {
-                // filteredByDistance = allItems.filter(item => {
-                //     if (item.zipcode === parseInt(zip_code.zip_code)) {
-                //         return item
-                //     }
-                // });
-                items.forEach(item => {
-                    if (item.zipcode === parseInt(zip_code.zip_code)) {
-                        distanceArray.push(item);
-                    }
+        if (e.target.value !== "none") {
+            ZipAPI.getZipCodesByRadius(location, e.target.value).then(data=>{
+                data.data.zip_codes.forEach(zip_code => {
+                    allItems.forEach(item => {
+                        if (item.zipcode === parseInt(zip_code.zip_code)) {
+                            distanceArray.push(item);
+                        }
+                    })
                 })
+                setItems(distanceArray);
             })
-            setItems(distanceArray);
-            // console.log(distanceArray)
-        })
+        } else {
+            setItems(allItems)
+            document.getElementById("distance-filter").value = "";
+        }
     }
 
     return (
