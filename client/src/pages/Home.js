44de5ba295bf66e4be615b8da379be72d7b0105c
Fernando Angulo Donoso <fangulo82@gmail.com>
2020-11-21 21:18:31 -0800

    Adding ZipAPI.js file

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect, useContext } from "react";
 import HomeFeed from "../components/HomeFeed";
 import HomeFilterForm from "../components/HomeFilterForm";
 import HomeSortForm from "../components/HomeSortForm";
-// import ItemCard from "../components/ItemCard";
+import HomeZipFilterForm from "../components/HomeZipFilterForm";
 import { AuthContext } from "../Context/AuthContext";
 import ItemAPI from "../utils/ItemAPI";
 import UserAPI from "../utils/UserAPI";
+import ZipAPI from "../utils/ZipAPI";
 
 function Home() {
     const [allItems, setAllItems] = useState([]);
@@ -16,6 +17,8 @@ function Home() {
     const [filterParam, setFilterParam] = useState('');
     const [sortString, setSortString] = useState('');
     const {id, setId, photo, setPhoto} = useContext(AuthContext);
+    const [location, setLocation] = useState("");
+    const [distance, setDistance] = useState("");
 
     useEffect(() => {
         ItemAPI.getAllItems().then(data => {
@@ -111,6 +114,193 @@ function Home() {
         }
     }
 
+    function handleLocationInputChange(e) {
+        setLocation(e.target.value)
+    }
+    // const zipcodeData = [
+    //         {
+    //             zip_code: "92659",
+    //             distance: 8.77,
+    //             city: "Newport Beach",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92663",
+    //             distance: 8.145,
+    //             city: "Newport Beach",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92660",
+    //             distance: 9.842,
+    //             city: "Newport Beach",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92628",
+    //             distance: 8.071,
+    //             city: "Costa Mesa",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92627",
+    //             distance: 7.297,
+    //             city: "Costa Mesa",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92615",
+    //             distance: 5.782,
+    //             city: "Huntington Beach",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92646",
+    //             distance: 4.812,
+    //             city: "Huntington Beach",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92648",
+    //             distance: 3.07,
+    //             city: "Huntington Beach",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92626",
+    //             distance: 6.503,
+    //             city: "Costa Mesa",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92707",
+    //             distance: 7.989,
+    //             city: "Santa Ana",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92605",
+    //             distance: 1.039,
+    //             city: "Huntington Beach",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92728",
+    //             distance: 5.185,
+    //             city: "Fountain Valley",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92799",
+    //             distance: 6.323,
+    //             city: "Santa Ana",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92735",
+    //             distance: 9.167,
+    //             city: "Santa Ana",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92708",
+    //             distance: 3.406,
+    //             city: "Fountain Valley",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "90742",
+    //             distance: 3.945,
+    //             city: "Sunset Beach",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92704",
+    //             distance: 5.961,
+    //             city: "Santa Ana",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92647",
+    //             distance: 0,
+    //             city: "Huntington Beach",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92649",
+    //             distance: 2.474,
+    //             city: "Huntington Beach",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "90743",
+    //             distance: 4.681,
+    //             city: "Surfside",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92702",
+    //             distance: 7.982,
+    //             city: "Santa Ana",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92655",
+    //             distance: 2.052,
+    //             city: "Midway City",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92703",
+    //             distance: 6.058,
+    //             city: "Santa Ana",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92701",
+    //             distance: 8.797,
+    //             city: "Santa Ana",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "90853",
+    //             distance: 7.221,
+    //             city: "Long Beach",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92685",
+    //             distance: 2.148,
+    //             city: "Westminster",
+    //             state: "CA"
+    //         },
+    //         {
+    //             zip_code: "92712",
+    //             distance: 8.179,
+    //             city: "Santa Ana",
+    //             state: "CA"
+    //         }
+    //  ]
+    function handleDistanceInputChange(e) {
+        let distanceArray = [];
+        ZipAPI.getZipCodesByRadius(location, e.target.value).then(data=>{
+            data.data.zip_codes.forEach(zip_code => {
+                // filteredByDistance = allItems.filter(item => {
+                //     if (item.zipcode === parseInt(zip_code.zip_code)) {
+                //         return item
+                //     }
+                // });
+                items.forEach(item => {
+                    if (item.zipcode === parseInt(zip_code.zip_code)) {
+                        distanceArray.push(item);
+                    }
+                })
+            })
+            setItems(distanceArray);
+            // console.log(distanceArray)
+        })
+    }
+
     return (
         <div>
             <div className = "container">
@@ -124,6 +314,10 @@ function Home() {
                 <HomeSortForm
                     onSortChange = {handleSortChange}
                 ></HomeSortForm>
+                <HomeZipFilterForm
+                    onLocationChange = {handleLocationInputChange}
+                    onDistanceChange = {handleDistanceInputChange}
+                ></HomeZipFilterForm>
             </div>
             <HomeFeed
                 items = {items}
@@ -131,4 +325,5 @@ function Home() {
         </div>
     );
 }
+
 export default Home;
---
+++ client/src/utils/ZipAPI.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const API_KEY = "e2wXC2AwQmTyIBIYRs4g1enr5Rs6E8KSQuLYTWMxmkAIjUfVEIeCPeAlJg5aGul6";
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const ZipAPI = {
+    getZipCodesByRadius: function(zipcode, distance) {
+        return axios({
+            mode: "no-cors",
+            method: "get",
+            url: proxyurl + `https://www.zipcodeapi.com/rest/${API_KEY}/radius.json/${zipcode}/${distance}/miles`,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        });
+    }
+}
+export default ZipAPI;
