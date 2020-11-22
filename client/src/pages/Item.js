c2f9cf59f0d4ada94f0cf01c1917ec4940385e1c
Jthui95 <jonathanhui.hk@gmail.com>
2020-11-21 23:34:15 -0800

    stylized our item cards

--- client/src/pages/Item.js
+++ client/src/pages/Item.js
@@ -1,15 +1,20 @@
-import React, {useEffect, useState, useContext} from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import ItemAPI from "../utils/ItemAPI";
 import UserAPI from "../utils/UserAPI";
-import {AuthContext} from "../Context/AuthContext";
+import { AuthContext } from "../Context/AuthContext";
+import "../components/ItemsPage/style.css"
 
-function Home() {
+
+
+
+
+function Item() {
     const [item, setItem] = useState({});
     const [user, setUser] = useState({});
     const authContext = useContext(AuthContext);
 
-    const {id} = useParams()
+    const { id } = useParams()
 
     useEffect(() => {
         ItemAPI.getItem(id)
@@ -22,32 +27,44 @@ function Home() {
             .catch(err => console.log(err.response));
     }, [])
 
-    return (
-        <div>
-            <h1>{item.title}</h1>
-            {item.photo ?
-                <img src={item.photo} alt={item.title} className="img-fluid img-thumbnail" style={{height:'25%', width: '25%'}}/> :
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png" alt={item.title} className="img-fluid img-thumbnail" style={{height:'25%', width: '25%'}}/>
-            }
-            <p>{`details: ${item.details}`}</p>
-            <p>{`condition: ${item.condition}`}</p>
-            <p>{`user: ${item.user}`}</p>
-            <p>{`value: $${item.value}`}</p>
-            <p>{`zipcode: ${item.zipcode}`}</p>
 
-            <div className="card shadow" style={{width: "18rem"}}>
-                {user.photo ? 
-                    <img src={user.photo} className="card-img-top img-fluid img-thumbnail" alt={user.username}/> :
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png" className="card-img-top img-fluid img-thumbnail" alt={user.username}/>
-                }
-                <div className="card-body">
-                    <h4 className="card-title">{user.username}</h4>
-                    <h6 className="card-title">{user.first_name} {user.last_name}</h6>
-                    <p className="card-text">{user.email}</p>
+    return (
+
+        <div>
+            <div className="grid-container">
+                <div className="row">
+                    <div className="card" style={{ width: "18rem" }}>
+                        {item.photo ?
+                            <img src={item.photo} alt={item.title} className="img-fluid img-thumbnail"/> :
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png" alt={item.title} className="img-fluid img-thumbnail" />
+                        }
+                        <h5>{item.title}</h5>
+                        <p className="card-text">{`Details: ${item.details}`}</p>
+                        <p className="card-text">{`Condition: ${item.condition}`}</p>
+                        <p className="card-text">{`User: ${item.user}`}</p>
+                        <p className="card-text">{`Value: $${item.value}`}</p>
+                        <p className="card-text">{`Zipcode: ${item.zipcode}`}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="grid-container">
+                <div className="row">
+                    <div className="card" style={{ width: "18rem" }}>
+                        {user.photo ?
+                            <img src={user.photo} className="card-img-top img-fluid img-thumbnail" alt={user.username} /> :
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png" className="card-img-top img-fluid img-thumbnail" alt={user.username} />
+                        }
+                        <div className="card-body">
+                            <p className="card-text">{user.username}</p>
+                            <p className="card-text">{user.first_name} {user.last_name}</p>
+                            <p className="card-text">{user.email}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
 
-export default Home;
+export default Item;
