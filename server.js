5974e78c1dadff27de65b5635b10540b45fa372a
Fernando Angulo Donoso <fangulo82@gmail.com>
2020-11-23 00:06:13 -0800

    server.js modified

--- server.js
+++ server.js
@@ -38,6 +38,13 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
 }
 
 // Connect to the Mongo DB
