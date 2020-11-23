7ff68a0eca20bb8e9ae10eeeeb65bf853c959fcc
Maxx Sanner <maxxsanner105@gmail.com>
2020-11-23 00:13:13 -0800

    server.js change to fix heroku issues

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get('/*', function(req, res) {
+  app.get(function(req, res) {
     res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
       if (err) {
         res.status(500).send(err)
