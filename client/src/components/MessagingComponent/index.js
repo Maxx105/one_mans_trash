b47f4491d4a55a788ad05357104caecbb1791077
Maxx Sanner <maxxsanner105@gmail.com>
2020-12-09 17:07:48 -0800

    added slight messaging style

--- client/src/components/MessagingComponent/index.js
+++ client/src/components/MessagingComponent/index.js
@@ -8,8 +8,11 @@ function MessagingComponent(props) {
     <div>
         <ul className="list-group">
             {props.users.map((user) => (
-                <Link to={"/messages/" + user.id} key = {user.id}>
-                    <li className="list-group-item" id="messageUserlist" onClick = {props.conversationSelect}>{user.user} - {user.item}</li>
+                <Link to={"/messages/" + user.id} key = {user.id} id="message-user">
+                    <li className="list-group-item" id="messageUserlist" onClick = {props.conversationSelect}>
+                        {/* <img src={user.photo} width="55" height="55" className="img-thumbnail rounded-circle" style={{marginRight: "10px"}}/> */}
+                        <strong>{user.user}</strong> - {user.item}
+                    </li>
                 </Link>
             ))}
         </ul>
